968302309919aa2d3a04716f355f81dcff551c48
Mehmet Hasbi KAYNAK <73940626+BrOlaf45@users.noreply.github.com>
2021-02-02 23:56:13 +0100

    Add MainForm

--- MyPhoneDirectoryProject/MyPhoneDirectoryProject.WFUI/MainForm.Designer.cs
+++ MyPhoneDirectoryProject/MyPhoneDirectoryProject.WFUI/MainForm.Designer.cs
@@ -29,12 +29,293 @@ namespace MyPhoneDirectoryProject.WFUI
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_name = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_surname = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_phonei = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt_phoneii = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txt_phoneiii = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txt_email = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txt_website = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txt_address = new System.Windows.Forms.TextBox();
+            this.btn_registry = new System.Windows.Forms.Button();
+            this.txt_description = new System.Windows.Forms.TextBox();
+            this.groupBox2.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(254, 532);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Directory List";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btn_registry);
+            this.groupBox2.Controls.Add(this.tabControl1);
+            this.groupBox2.Location = new System.Drawing.Point(272, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(417, 532);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "New Directory Registry";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(6, 19);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(402, 469);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.txt_address);
+            this.tabPage1.Controls.Add(this.label8);
+            this.tabPage1.Controls.Add(this.txt_website);
+            this.tabPage1.Controls.Add(this.label7);
+            this.tabPage1.Controls.Add(this.txt_email);
+            this.tabPage1.Controls.Add(this.label6);
+            this.tabPage1.Controls.Add(this.txt_phoneiii);
+            this.tabPage1.Controls.Add(this.label5);
+            this.tabPage1.Controls.Add(this.txt_phoneii);
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.txt_phonei);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.txt_surname);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.txt_name);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(394, 443);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Personel Informations";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.txt_description);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(394, 443);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Description";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // txt_name
+            // 
+            this.txt_name.Location = new System.Drawing.Point(92, 17);
+            this.txt_name.Name = "txt_name";
+            this.txt_name.Size = new System.Drawing.Size(279, 20);
+            this.txt_name.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Surname";
+            // 
+            // txt_surname
+            // 
+            this.txt_surname.Location = new System.Drawing.Point(92, 53);
+            this.txt_surname.Name = "txt_surname";
+            this.txt_surname.Size = new System.Drawing.Size(279, 20);
+            this.txt_surname.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 92);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Phone I";
+            // 
+            // txt_phonei
+            // 
+            this.txt_phonei.Location = new System.Drawing.Point(92, 89);
+            this.txt_phonei.Name = "txt_phonei";
+            this.txt_phonei.Size = new System.Drawing.Size(279, 20);
+            this.txt_phonei.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(28, 125);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Phone II";
+            // 
+            // txt_phoneii
+            // 
+            this.txt_phoneii.Location = new System.Drawing.Point(92, 122);
+            this.txt_phoneii.Name = "txt_phoneii";
+            this.txt_phoneii.Size = new System.Drawing.Size(279, 20);
+            this.txt_phoneii.TabIndex = 1;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(28, 160);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(50, 13);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Phone III";
+            // 
+            // txt_phoneiii
+            // 
+            this.txt_phoneiii.Location = new System.Drawing.Point(92, 157);
+            this.txt_phoneiii.Name = "txt_phoneiii";
+            this.txt_phoneiii.Size = new System.Drawing.Size(279, 20);
+            this.txt_phoneiii.TabIndex = 1;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(28, 193);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(32, 13);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Email";
+            // 
+            // txt_email
+            // 
+            this.txt_email.Location = new System.Drawing.Point(92, 190);
+            this.txt_email.Name = "txt_email";
+            this.txt_email.Size = new System.Drawing.Size(279, 20);
+            this.txt_email.TabIndex = 1;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(28, 232);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(46, 13);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "Website";
+            // 
+            // txt_website
+            // 
+            this.txt_website.Location = new System.Drawing.Point(92, 229);
+            this.txt_website.Name = "txt_website";
+            this.txt_website.Size = new System.Drawing.Size(279, 20);
+            this.txt_website.TabIndex = 1;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(28, 268);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(45, 13);
+            this.label8.TabIndex = 0;
+            this.label8.Text = "Address";
+            // 
+            // txt_address
+            // 
+            this.txt_address.Location = new System.Drawing.Point(92, 265);
+            this.txt_address.Multiline = true;
+            this.txt_address.Name = "txt_address";
+            this.txt_address.Size = new System.Drawing.Size(279, 172);
+            this.txt_address.TabIndex = 1;
+            // 
+            // btn_registry
+            // 
+            this.btn_registry.Location = new System.Drawing.Point(13, 494);
+            this.btn_registry.Name = "btn_registry";
+            this.btn_registry.Size = new System.Drawing.Size(391, 23);
+            this.btn_registry.TabIndex = 2;
+            this.btn_registry.Text = "New Registry";
+            this.btn_registry.UseVisualStyleBackColor = true;
+            // 
+            // txt_description
+            // 
+            this.txt_description.Location = new System.Drawing.Point(3, 6);
+            this.txt_description.Multiline = true;
+            this.txt_description.Name = "txt_description";
+            this.txt_description.Size = new System.Drawing.Size(388, 431);
+            this.txt_description.TabIndex = 0;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(690, 556);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "MainForm";
+            this.groupBox2.ResumeLayout(false);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btn_registry;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TextBox txt_address;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txt_website;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txt_email;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txt_phoneiii;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txt_phoneii;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txt_phonei;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_surname;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_name;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TextBox txt_description;
     }
 }
