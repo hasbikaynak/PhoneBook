d9cf8f2e6e8f1b0ba136d79862aa1ff1f1c86dbe
Mehmet Hasbi KAYNAK <73940626+BrOlaf45@users.noreply.github.com>
2021-02-05 02:01:30 +0100

    add LicenseScreen

---
+++ MyPhoneDirectoryProject/MyPhoneDirectoryProject.WFUI/LicenseScreen.Designer.cs
@@ -0,0 +1,135 @@
+﻿
+namespace MyPhoneDirectoryProject.WFUI
+{
+    partial class LicenseScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LicenseScreen));
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txt_licensenumber = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_enter = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txt_licensenumber);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.btn_enter);
+            this.groupBox2.Location = new System.Drawing.Point(240, 11);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox2.Size = new System.Drawing.Size(461, 271);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Licensing Process";
+            // 
+            // txt_licensenumber
+            // 
+            this.txt_licensenumber.Location = new System.Drawing.Point(43, 111);
+            this.txt_licensenumber.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txt_licensenumber.Name = "txt_licensenumber";
+            this.txt_licensenumber.Size = new System.Drawing.Size(414, 22);
+            this.txt_licensenumber.TabIndex = 0;
+            this.txt_licensenumber.Text = "9245fe4a-d402-451c-b9ed-9c1a04247482";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(234, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Please Enter Your License Number ";
+            // 
+            // btn_enter
+            // 
+            this.btn_enter.Location = new System.Drawing.Point(43, 155);
+            this.btn_enter.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btn_enter.Name = "btn_enter";
+            this.btn_enter.Size = new System.Drawing.Size(418, 32);
+            this.btn_enter.TabIndex = 2;
+            this.btn_enter.Text = "Update";
+            this.btn_enter.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 11);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox1.Size = new System.Drawing.Size(221, 271);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(3, 17);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(215, 252);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // LicenseScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(709, 290);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "LicenseScreen";
+            this.Text = "LicenseScreen";
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txt_licensenumber;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_enter;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
